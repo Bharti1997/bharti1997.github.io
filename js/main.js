4f01ecb5c8aaec1ecb9123572eec8f21a30b5e61
Bharti1997 <bhartidrall1997@gmail.com>
2022-10-29 15:36:31 +0530

    fix

--- js/main.js
+++ js/main.js
@@ -27,6 +27,14 @@
         }
     });
 
+    $(window).load(function() {
+            $('.portfolio-container').isotope({
+            // options
+            itemSelector: '..portfolio-item',
+            layoutMode: 'fitRows'
+        });
+    });
+
 
     // Modal Video
     $(document).ready(function () {
@@ -43,22 +51,6 @@
         $('#videoModal').on('hide.bs.modal', function (e) {
             $("#video").attr('src', $videoSrc);
         })
-
-        // Portfolio isotope and filter
-        var portfolioIsotope = $('.portfolio-container').isotope({
-            itemSelector: '.portfolio-item',
-            layoutMode: 'fitRows'
-        });
-        $('#portfolio-flters li').on('click', function () {
-            $("#portfolio-flters li").removeClass('active');
-            $(this).addClass('active');
-    
-            portfolioIsotope.isotope({filter: $(this).data('filter')});
-        });
-
-        $('.portfolio-container').imagesLoaded(function(){
-            $('.portfolio-container').isotope('reloadItems');
-        });
     });
 
 
